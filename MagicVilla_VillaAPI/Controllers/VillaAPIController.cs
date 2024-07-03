c3854904b1a7be866135c1f83773e3277ed07feb
Krishna Chaitanya Chigurupati <chaitu1323@outlook.com>
2024-07-03 16:30:40 -0400

    FirstCheck

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    public class VillaAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
