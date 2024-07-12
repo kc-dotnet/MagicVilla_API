b8b86a6dd91d90e5bd395fc4f268ce73f17a149e
Krishna Chaitanya Chigurupati <chaitu1323@outlook.com>
2024-07-12 18:11:34 -0400

    Created two get endpoints

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models; 
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    public class VillaAPIController : Controller
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return View();
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
         }
     }
 }
